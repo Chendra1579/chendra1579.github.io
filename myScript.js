43ecb8f6bde66c71b9b5db57631f3d8104183067
Chendra1579 <43216021+Chendra1579@users.noreply.github.com>
2018-10-17 08:31:55 -0500

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -23,72 +23,96 @@ function temp()
         return "Winter";
     }
 }
-function workOut()
+function working()
     {
-         var work = document.project2.type.getSelection.value;
-        var min= Number(document.project2.Time.value);
+        var work = document.project2.cal.value;
+        var min= Number(document.project2.TimeNum.value);
         //create logic that outputs "hello world" different depending on language selected
         if (work == 'jog' ) //case of jog
         {
-            if (min>=1)
-            {
-                return min*5 + " Cal"
-            }
+                return min*5 + " Cal";
         }
         else if (work == 'swim')
         {
-            if (min>=1)
-            {
-                return min*4 + " Cal"
-            }
+                return min*4 + " Cal";
         }
-        else if (work =='golf')
+        else if (work == 'golf')
         {
-            if (min>=1)
-            {
-                return min*2 + " Cal"
-            }
+                return min*2 + " Cal";
         }
         else if(work == 'VS')
         {
-            if (min>=1)
-            {
-                return min*1 + " Cal"
-            }
+                return min + " Cal";
         }
         else if ( work == 'baseball')
         {
-            if (min>=2)
-            {
-                return min*2 + " Cal"
-            }
+                return min*3 + " Cal";
         }
         else
         {
-            return "please choose a sport"
+            return "please choose a sport";
         }
 
     }
-function grade(Number) {
-    var GRADENUM = Number(document.project2.Num.value);
-    //create logic that outputs grade different depending on number selected
-    if (GRADENUM >= 90) {
+function grade()
+{
+    var num5 = Number(document.project3.percentage.value);
+    //create logic that outputs temperature different depending on number selected
+    if (num5 >= 110) {
+        return "A++";
+    }
+    else if (num5 >= 90) {
         return "A";
     }
-    else if (GRADENUM >= 80) {
+    else if (num5 >= 80) {
         return "B";
     }
-    else if (GRADENUM >= 70) {
+    else if (num5 >= 70) {
         return "C";
     }
-    else if (GRADENUM >= 60) {
+    else if (num5 >= 60) {
         return "D";
     }
-    else if (GRADENUM) {
-        return "F+";
-    }
-    else {
-        NC
+    else
+    {
+        return "F";
     }
 }
+function isPrime()
+{
+    /*var x = 7;
+    var y = 3;
+    var z =x%y;
+    alert(z);
+    */
+    var checkNum = document.primeNumber.pNum.value;
+    var checkPrime=true; // this is a boolean variable aja always set to true or false
+
+    if (checkNum<=1)
+    {
+        alert(checkNum + "is NOT prime")
+        return
+    }
+    for (var i=checkNum-1; i>1; i--)
+    {
+        if (checkNum%i==0) // means no remainder
+        {
+            checkPrime=false;
+        }
+    }
+    //After the ;loop completes, display a message based on the value of check prime
+    if (checkPrime== true)
+    {
+        alert(checkNum + " isPrime")
+    }
+    else
+    {
+        alert(checkNum+" is not prime")
+    }
+}
+/*
+from javaScript FUN
+ */
+
+
 
