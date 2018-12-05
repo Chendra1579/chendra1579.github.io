ed0fd3c7e1ab42f31cb6ddb4434b652b75d2f0fd
Chendra1579 <43216021+Chendra1579@users.noreply.github.com>
2018-12-05 12:30:19 -0600

    Add files via upload

--- quizScript.js
+++ quizScript.js
@@ -7,42 +7,48 @@ function checkAns()
     var score=0;
     //check answers
     var q1answer= document.quiz.a1.value.toLowerCase();
-    if (q1answer === 'sora')
+    if (q1answer === '')
     {
-        score = score + 1;
-        feedback.push("CORRECT!")
+        alert("please fill in input 1")
+
     }
-    //push feedback to the array
-    else
-    {
-        feedback.push("wrong, The answer was sora")
-    }
-    //check our work
-    //check A2
-    if (document.quiz.a2.value ==='crepe')
-    {
-        //increase the score
-        score++;
-        feedback.push("question 2 is correct!")
-    }else{
-        feedback.push("wrong, the answer is crepe")
-    }
-    //check A3
-    if (document.quiz.ans3a.checked === true && document.quiz.ans3b.checked === true&& document.quiz.ans3d.checked === true)
-    {
-        if (document.quiz.ans3c.checked === true || document.checked=== true)
-        {
-            feedback.push("Wrong!Question 3 was Hong Kong/California/Japan")
-        }else{
-            score++;
-            feedback.push("correct")
+    else {
+
+
+        if (q1answer === 'sora') {
+            score = score + 1;
+            feedback.push("CORRECT!")
         }
-    }else{
-        feedback.push("Wrong!Question 3 was Hong Kong/California/Japan")
+        else {
+            feedback.push("wrong, The answer was sora")
+        }
+
+        //push feedback to the array
+        //check our work
+        //check A2
+        if (document.quiz.a2.value === 'crepe') {
+            //increase the score
+            score++;
+            feedback.push("question 2 is correct!")
+        } else {
+            feedback.push("wrong, the answer is crepe")
+        }
+        //check A3
+        if (document.quiz.ans3a.checked === true && document.quiz.ans3b.checked === true && document.quiz.ans3d.checked === true) {
+            if (document.quiz.ans3c.checked === true || document.checked === true) {
+                feedback.push("Wrong!Question 3 was Hong Kong/California/Japan")
+            } else {
+                score++;
+                feedback.push("correct")
+            }
+        } else {
+            feedback.push("Wrong!Question 3 was Hong Kong/California/Japan")
+        }
+        return"Answers:"+ feedback + "                  Score: "+ score
+
+
+        //check work
+        //alert(feedback);
+        //alert(score);
     }
-
-
-    //check work
-    alert(feedback);
-    alert(score);
 }
